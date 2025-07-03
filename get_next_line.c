230f92d8078a31f4b0a05c2c3bd981a39768de8b
tisonpatrik <patriktison@gmail.com>
2025-07-03 16:57:40 +0200

    better approach

--- get_next_line.c
+++ get_next_line.c
@@ -1,118 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ptison <ptison@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 16:32:03 by ptison            #+#    #+#             */
-/*   Updated: 2025/07/03 16:32:27 by ptison           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-void	append(t_list **list, char *buf);
-void	polish_list(t_list **list);
-void	ft_copy_str(t_list *list, char *str);
+int		has_next_line(t_buffer *buffer);
+char	*extract_line(char *buff, char **next_line, int s_buff, int pos);
+char	*concat(char *buff, char *addme, size_t s_buff, size_t s_addme);
+int		read_next(int file_descriptor, t_buffer *b, char **new_line);
 
-int	ft_found_newline(t_list *list)
+// Global buffer array definition
+// Stores buffered data for each file descriptor (up to MAX_FD)
+// Each file descriptor gets its own buffer to maintain state between calls
+t_buffer g_file_buffers[MAX_FD] = {0};
+
+char	*get_next_line(int file_descriptor)
 {
-	int	i;
+	char			*new_line;
+	int				newline_position;
 
-	if (NULL == list)
-		return (0);
-	while (list)
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	newline_position = -1;
+	while (1)
 	{
-		i = 0;
-		while (list->str_buf[i] && i < BUFFER_SIZE)
+		newline_position = has_next_line(&g_file_buffers[file_descriptor]);
+		new_line = NULL;
+		if (newline_position >= 0)
 		{
-			if (list->str_buf[i] == '\n')
-				return (1);
-			++i;
+			g_file_buffers[file_descriptor].buffer_data = extract_line(g_file_buffers[file_descriptor].buffer_data, &new_line, g_file_buffers[file_descriptor].buffer_size, newline_position);
+			g_file_buffers[file_descriptor].buffer_size -= newline_position + 1;
+			break ;
 		}
-		list = list->next;
+		if (!read_next(file_descriptor, &g_file_buffers[file_descriptor], &new_line))
+			break ;
 	}
-	return (0);
-}
-
-void	create_list(t_list **list, int fd)
-{
-	int		char_read;
-	char	*buf;
-
-	while (!ft_found_newline(*list))
-	{
-		buf = malloc(BUFFER_SIZE + 1);
-		if (NULL == buf)
-			return ;
-		char_read = read(fd, buf, BUFFER_SIZE);
-		if (!char_read)
-		{
-			free(buf);
-			return ;
-		}
-		buf[char_read] = '\0';
-		append(list, buf);
-	}
-}
-
-int	len_to_newline(t_list *list)
-{
-	int	i;
-	int	len;
-
-	if (NULL == list)
-		return (0);
-	len = 0;
-	while (list)
-	{
-		i = 0;
-		while (list->str_buf[i])
-		{
-			if (list->str_buf[i] == '\n')
-			{
-				++len;
-				return (len);
-			}
-			++i;
-			++len;
-		}
-		list = list->next;
-	}
-	return (len);
-}
-
-char	*get_line(t_list *list)
-{
-	int		str_len;
-	char	*next_str;
-
-	if (NULL == list)
-		return (NULL);
-	str_len = len_to_newline(list);
-	next_str = malloc(str_len + 1);
-	if (NULL == next_str)
-		return (NULL);
-	ft_copy_str(list, next_str);
-	return (next_str);
-}
-
-char	*get_next_line(int fd)
-{
-	static t_list	*list = NULL;
-	char			*next_line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
-		return (NULL);
-	create_list(&list, fd);
-	if (list == NULL)
-		return (NULL);
-	next_line = get_line(list);
-	polish_list(&list);
-	return (next_line);
+	return (new_line);
 }
