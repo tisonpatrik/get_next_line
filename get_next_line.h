d452bf601d181c9facfc032d03adf137754ac8cd
Patrik Tison <ptison@c2r4s5.42prague.com>
2025-07-03 16:33:03 +0200

    normi

--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptison <ptison@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 16:32:06 by ptison            #+#    #+#             */
+/*   Updated: 2025/07/03 16:32:36 by ptison           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
 
-#endif
-typedef struct s_list {
-  char *str_buf;
-  struct s_list *next;
-} t_list;
+# endif
 
-char *get_next_line(int fd);
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
+
+char				*get_next_line(int fd);
 
 #endif
